42da2d73502111745829301518840a11309c222f
tatiana-afanasieva <51439538+tatiana-afanasieva@users.noreply.github.com>
2019-06-12 16:25:48 +0900

    Add files via upload

---
+++ laby tatiana/4444.cpp
@@ -0,0 +1,17 @@
+// 4444.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include "LFOUR.h"
+#include <iostream>
+using namespace std;
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	int a;
+	a= ReadNum();
+	ReadNum();
+	std::cout<<a;
+	return 0;
+}
+
---
+++ laby tatiana/LFOUR.h
@@ -0,0 +1,10 @@
+#ifndef _LFOUR_H_
+#define _LFOUR_H_
+
+#include <iostream>
+#include <stdio.h>
+#include <windows.h>
+
+extern "C" __declspec(dllexport) int ReadNum();
+
+#endif
